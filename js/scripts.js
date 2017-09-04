e145d26add7b75c9fbd37931c5c589376e18320c
Aleksander Chromik <achromik@gmail.com>
2017-09-04 02:06:11 +0200

    some small fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,19 @@
-document.querySelectorAll('img.svg').forEach(function(img){
-    var imgID = img.id;
-    var imgClass = img.className;
-    var imgURL = img.src;
+// auto collapse menu on mobile view after clicking on link
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') ) {
+        $(this).collapse('hide');
+    }
+});
 
-    fetch(imgURL).then(function(response) {
-        return response.text();
-    }).then(function(text){
 
-        var parser = new DOMParser();
-        var xmlDoc = parser.parseFromString(text, "text/xml");
+//hide navbar when scrolldown
+$(window).scroll(function() {
 
-        // Get the SVG tag, ignore the rest
-        var svg = xmlDoc.getElementsByTagName('svg')[0];
-
-        // Add replaced image's ID to the new SVG
-        if(typeof imgID !== 'undefined') {
-            svg.setAttribute('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if(typeof imgClass !== 'undefined') {
-            svg.setAttribute('class', imgClass+' replaced-svg');
-        }
-
-        // Remove any invalid XML tags as per http://validator.w3.org
-        svg.removeAttribute('xmlns:a');
-
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if(!svg.getAttribute('viewBox') && svg.getAttribute('height') && svg.getAttribute('width')) {
-            svg.setAttribute('viewBox', '0 0 ' + svg.getAttribute('height') + ' ' + svg.getAttribute('width'));
-        }
-
-        // Replace image with new SVG
-        img.parentNode.replaceChild(svg, img);
-
-    });
+    if ($(this).scrollTop()>25) {
+            $('.navbar').fadeOut();
+    }
+    else {
+        $('.navbar').fadeIn();
+    }
 
 });
